5b237fea3875b64e08c79940155447845e7744e4
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-18 19:11:38 +0600

    module-26-8-recap-reusable function

---
+++ script/ellipse.js
@@ -0,0 +1,19 @@
+function calculateEllipseArea() {
+    const base = getInputValueById('ellipse-base')
+    const height = getInputValueById('ellipse-height')
+
+    const area = 3.14159265359 * base * height;
+
+    setInnerTextById('ellipse-area', area)
+}
+
+function getInputValueById(inputFieldById) {
+    const inputField = document.getElementById(inputFieldById);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+function setInnerTextById(elementId, area) {
+    const element = document.getElementById(elementId)
+    element.innerText = area;
+}

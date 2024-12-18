2971ca3655f1995d41329bc137a1c6779656c6c5
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-18 15:57:06 +0600

    module-26-5-commit

--- script/rectangle.js
+++ script/rectangle.js
@@ -1,3 +1,22 @@
+//get rectangle width value
 function calculateRectangleArea() {
-    console.log('...')
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidthInput.value;
+    const width = parseFloat(rectangleWidthText)
+    // console.log(width)
+
+    //get rectangle Length value
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    // console.log(rectangleLengthText)
+    const length = parseFloat(rectangleLengthText)
+    // console.log(length)
+
+    //calculate rectangle
+    const area = width * length;
+
+    // display rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
+

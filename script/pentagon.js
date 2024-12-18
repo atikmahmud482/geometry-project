c5784c12b3ae3c9f1524e2ed085dec8ec94d95d1
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-18 18:17:10 +0600

    module-26-6-commoncodepart1

---
+++ script/pentagon.js
@@ -0,0 +1,11 @@
+function calculatePentagonArea() {
+    const base = getPentagonBase();
+    console.log(base)
+}
+
+// function getPentagonBase() {
+//     const baseInput = document.getElementById('pentagon-base');
+//     const baseText = baseInput.value;
+//     const base = parseFloat(baseText);
+//     return base;
+// }

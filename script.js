955bdb834badbc0dce74985d0c6eead2a5602aa2
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-18 13:09:52 +0600

    module-26-3-commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function calculateTriangleArea() {
-    console.log('click')
+    // get triangle base value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText)
+    // console.log(base)
+
+
+    //get triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText)
 }
+
